d2be7560deb601222976ec8b9dc434e5a3103a9a
Alecar Bilgenoglu <alechnoble@gmail.com>
2023-11-01 10:24:29 +0000

    v

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,6 +1,6 @@
-SELECT 
-(quantity * purchase_price) AS purchase_cost
-, (revenue - purchase_cost)) AS margin
+SELECT *
+    , CAST((quantity * CAST(purchase_price AS FLOAT64)) AS FLOAT64)AS purchase_cost
+    , CAST((revenue - (quantity * CAST(purchase_price AS FLOAT64)) AS FLOAT64,2) AS margin
 FROM {{ref("stg_raw__sales")}} s
-LEFT JOIN {{ref("stg_raw__product")}} p
-    USING (products_id)
+JOIN {{ref("stg_raw__product")}} p
+USING (products_id)
