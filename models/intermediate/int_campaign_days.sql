1865324dc9f24d2d4b28ec65c75e13cc86d2a739
Alecar Bilgenoglu <alechnoble@gmail.com>
2023-11-01 22:17:44 +0000

    campaign days

---
+++ models/intermediate/int_campaign_days.sql
@@ -0,0 +1,9 @@
+select
+    date_date,
+    round(sum(ads_cost), 2) as ad_cost,
+    round(sum(impression), 0) as ad_impression,
+    round(sum(click),2) as ad_clicks
+
+from {{ ref("int_campaigns") }}
+group by date_date
+order by date_date desc
